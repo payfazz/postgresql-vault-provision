2772c5304752543ade3bd5e05e435bbb855a8990
fadhilthomas <mthomasfadhilayahya@gmail.com>
2021-12-16 14:05:37 -0500

    add exporter roles

--- setup-roles.sql
+++ setup-roles.sql
@@ -6,6 +6,8 @@ CREATE ROLE apps;
 
 CREATE ROLE engineers;
 
+CREATE ROLE exporter;
+
 CREATE ROLE v_admin WITH CREATEROLE CREATEDB INHERIT LOGIN;
 
 CREATE ROLE vault_postgres WITH CREATEROLE LOGIN;
@@ -14,6 +16,4 @@ CREATE ROLE migration;
 
 GRANT migration TO postgres, v_admin, apps, engineers;
 
-GRANT rds_superuser to v_admin;
-
-GRANT rds_superuser to vault_postgres;
+GRANT rds_superuser to v_admin, vault_postgres, exporter;
